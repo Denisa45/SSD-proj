96dfa714c41dc38046f0384fb3cfd1e8a365bd74
Denisa45 <berzavadeniabeatrice.yahoo.com>
2025-10-22 22:34:17 +0300

    changed navigation

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { RouterLink } from '@angular/router';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import AddCourseModalComponent from '../../add-course-modal/add-course-modal';
+import {of, Observable } from 'rxjs';
+import { Firestore,collection,collectionData } from '@angular/fire/firestore';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -11,20 +13,33 @@ import AddCourseModalComponent from '../../add-course-modal/add-course-modal';
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.css']
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
   user: any = null;
-
-  constructor(
+  activeTab: string='home';
+  courses$: Observable<any[]> = of([]);
+  
+    constructor(
     private authService: AuthService,
-    private dialog: MatDialog            // ✅ injected here
+    private dialog: MatDialog,            // ✅ injected here
+    private firestore:Firestore
   ) {}
 
   ngOnInit() {
     const userData = localStorage.getItem('user');
     if (userData) {
       this.user = JSON.parse(userData);
+
+    // 🔹 Load courses from Firestore
+    const coursesCollection = collection(this.firestore, 'courses');
+    this.courses$ = collectionData(coursesCollection, { idField: 'id' });
     }
   }
+
+  // 🔹 Handle tab switching
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
+  
 
   logout() {
     this.authService.logout().catch((err: any) => console.warn('Firebase logout failed', err));
